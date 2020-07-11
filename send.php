ecb4890dfe8bca0483af0672dfc62d0a4168720f
max-mosquito <67696697+max-mosquito@users.noreply.github.com>
2020-07-11 14:18:41 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,14 +1,25 @@
-$fio = $_POST['fio'];
-$email = $_POST['email'];
-$fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
-echo $fio;
+<?php
+
+if(isset($_POST['submit'])){
+$to = "w-2aa@mail.ru";; // Здесь нужно написать e-mail, куда будут приходить письма
+$from = $_POST['email']; // this is the sender's Email address
+$first_name = $_POST['first_name'];
+$subject = "Форма отправки сообщений с сайта";
+$subject2 = "Copy of your form submission";
+$message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+$headers = "From:" . $from;
+$headers2 = "From:" . $to;
+
+mail($to,$subject,$message,$headers);
+// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
+echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+echo $first_name;
 echo "<br>";
-echo $email;
-mail("w-2aa@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n");
-if (mail("example@mail.ru", "Заказ с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
- {
-    echo "сообщение успешно отправлено";
-} else {
-    echo "при отправке сообщения возникли ошибки";
+echo $from;
+
 }
+
+?>
